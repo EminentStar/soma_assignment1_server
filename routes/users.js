b809a97a40e3edc34464d7717d48cb97f4cbd756
EminentStar <eminentstar@EminentStarui-MacBook-Pro.local>
2016-07-14 14:10:22 +0900

    mysql setting

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit: 3,
+  host:'localhost',
+  user: 'dev',
+  database: 'junkyu',
+  password: '0000'
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.  send('user ' + req.params.id);
+  pool.getConnection(function(err, connection){
+    connection.query('SELECT * FROM member', function(err, rows){
+      if(err) console.error("err : " + err);
+      console.log("rows: " + JSON.stringify(rows));
+      res.send(rows);
+      //res.render('user', {title: 'test', rows:rows});
+      connection.release();
+    });
+  });
 });
 
 module.exports = router;
